c0cc89d42ac7f3ef1b30fcbe9545db3aba7b31e5
Mirko Rajkovaca <mirko@mrcode.dev>
2020-02-10 09:13:38 +0100

    - added test for reproducing issue with qiskit aqua (when multiple jobs are spawned at once)

--- tests/test_toasterbackend.py
+++ tests/test_toasterbackend.py
@@ -95,7 +95,17 @@ class TestToasterBackend(unittest.TestCase):
         self.assertTrue(stats['statevector'] is None)
         self.assertNotEqual(stats['totalcounts'], stats_aer['totalcounts'])
 
-    
+    def test_multiple_jobs(self):
+        qc = self.get_bell_qc()
+        backend = ToasterBackend.ToasterBackend()
+        jobs = []
+        for i in range(1,50):
+            jobs.append(execute(qc,backend=backend,shots=1))
+        for job in jobs:
+            result = job.result()
+            counts = result.get_counts(qc)
+            self.assertEqual(len(counts), 1)
+
 
     @staticmethod
     def execute_and_get_stats(backend, qc, shots):
